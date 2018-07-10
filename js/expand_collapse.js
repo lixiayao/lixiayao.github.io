51393b9e5efe48ac892ed6f61ccf169dba0a7c2a
yueqi hu <hyq825@gmail.com>
2018-07-10 12:19:37 -0500

    roll back style

---
+++ js/expand_collapse.js
@@ -0,0 +1,9 @@
+function show()
+{
+//Function content goes here
+if(document.getElementById('expand').style.display == 'none')
+	document.getElementById('expand').style.display = 'block';
+else
+	document.getElementById('expand').style.display = 'none';
+
+}
